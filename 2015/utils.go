4ecbfa21f7bff839c0af0d0313df61da01ce4568
BoldiCzaun <boldizsar.czaun@gmail.com>
2024-09-05 15:23:56 +0200

    Minor refactor

--- 2015/utils.go
+++ 2015/utils.go
@@ -8,27 +8,6 @@ import (
 	"strconv"
 )
 
-func ReadInput() ([]string, error) {
-	file, err := os.Open("input.txt")
-	if err != nil {
-		fmt.Println("Error opening file:", err)
-		return nil, err
-	}
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	var lines []string
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
-
-	if err := scanner.Err(); err != nil {
-		fmt.Println("Error reading file:", err)
-		return nil, err
-	}
-	return lines, nil
-}
-
 func main() {
 	if len(os.Args) != 2 {
 		log.Fatal("Please provide exactly one day")
@@ -48,6 +27,27 @@ func main() {
 		log.Fatal("The day you have been asked for has not been implemented yet")
 	}
 
+}
+
+func ReadInput() ([]string, error) {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return nil, err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	var lines []string
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading file:", err)
+		return nil, err
+	}
+	return lines, nil
 }
 
 func PrintDiv() {
