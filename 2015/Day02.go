d88d9bfdf98f28f98e112e1d5dfb32e3cc2f5bee
BoldiCzaun <boldizsar.czaun@gmail.com>
2024-09-05 15:32:23 +0200

    Day 2 finished

--- 2015/Day02.go
+++ 2015/Day02.go
@@ -27,12 +27,43 @@ func Day02_main() {
 		log.Fatal(err)
 	}
 	D02_p1(lines)
+	D02_p2(lines)
 
 }
 
 func D02_p1(lines []string) {
 	fmt.Println("Part 1")
 
+	boxes := parse(lines)
+	sum := 0
+	for _, b := range boxes {
+		//surface
+		sum += 2 * (b.l*b.w + b.w*b.h + b.h*b.l)
+		// slack
+		sum += min(b.l*b.w, b.w*b.h, b.h*b.l)
+	}
+
+	fmt.Printf("%d sqrft of paper should be ordered\n", sum)
+	PrintDiv()
+}
+
+func D02_p2(lines []string) {
+	fmt.Println("Part 2")
+
+	boxes := parse(lines)
+	sum := 0
+	for _, b := range boxes {
+		ribbon := (b.l + b.h + b.w - max(b.l, b.h, b.w)) * 2
+		bow := b.l * b.h * b.w
+		sum += ribbon + bow
+	}
+
+	fmt.Printf("%d ft of ribbon is needed\n", sum)
+	PrintDiv()
+
+}
+
+func parse(lines []string) []box {
 	var boxes []box
 
 	for _, line := range lines {
@@ -46,13 +77,5 @@ func D02_p1(lines []string) {
 		boxes = append(boxes, newBox(l, w, h))
 	}
 
-	sum := 0
-	for _, b := range boxes {
-		//surface
-		sum += 2 * (b.l*b.w + b.w*b.h + b.h*b.l)
-		// slack
-		sum += min(b.l*b.w, b.w*b.h, b.h*b.l)
-	}
-
-	fmt.Printf("%d sqrft of paper should be ordered\n", sum)
+	return boxes
 }
