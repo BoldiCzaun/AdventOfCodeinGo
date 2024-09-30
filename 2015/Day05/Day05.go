bd103ab1eabd85b9a1621a85e4b9892f62e81761
BoldiCzaun <99271870+BoldiCzaun@users.noreply.github.com>
2024-09-30 13:38:14 +0200

    Update Day05.go

--- 2015/Day05/Day05.go
+++ 2015/Day05/Day05.go
@@ -4,25 +4,29 @@ import (
 	"aoc2015/utils"
 	"fmt"
 	"log"
-	"regexp"
 	"strings"
 )
 
 func Main() {
-	lines, err := utils.ReadInput("day05/input.txt")
+	lines, err := utils.ReadInput("Day05/input.txt")
 	if err != nil {
 		log.Panic(err)
 	}
 
 	nice_strings := 0
+	nice_strings2 := 0
 	for _, line := range lines {
 		// fmt.Print(line)
 		if d05_p1(line) {
 			nice_strings++
 		}
+		if d05_p2(line) {
+			nice_strings2++
+		}
 	}
 	fmt.Printf("Part 1:\nInput has %d nice strings\n", nice_strings)
 	utils.PrintDiv()
+	fmt.Printf("Part 2:\nInput has %d nice strings\n", nice_strings2)
 }
 
 func d05_p1(input string) (is_nice bool) {
@@ -35,6 +39,7 @@ func d05_p1(input string) (is_nice bool) {
 
 func d05_p2(input string) (is_nice bool) {
 	input = strings.ToLower(input)
+	is_nice = true
 	is_nice = has_pair_of_pairs(input)
 	is_nice = is_nice && has_repeat(input)
 	return
@@ -68,11 +73,7 @@ func has_pair_of_pairs(input string) bool {
 	for c := 'a'; c <= 'z'; c++ {
 		for d := 'a'; d <= 'z'; d++ {
 			st := string([]rune{c, d})
-			ts := string([]rune{d, c})
-			if c == d && strings.Count(input, st) >= 3 {
-				return true
-			}
-			if c != d && strings.Count(input, st) >= 2 && !strings.Contains(input, ts) {
+			if strings.Count(input, st) >= 2 {
 				return true
 			}
 		}
@@ -81,7 +82,10 @@ func has_pair_of_pairs(input string) bool {
 }
 
 func has_repeat(input string) bool {
-	pattern := `([a-z]).([a-z])`
-	matched, _ := regexp.MatchString(pattern, input)
-	return matched
+	for i := 0; i < len(input)-2; i++ {
+		if input[i] == input[i+2] {
+			return true
+		}
+	}
+	return false
 }
